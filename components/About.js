519e3a6a953b3ed42ec17c9ac88b2ae1a8f9548c
Akash S <92721700+akash130701@users.noreply.github.com>
2022-11-16 15:23:50 +0530

    Update About.js

--- components/About.js
+++ components/About.js
@@ -2,9 +2,9 @@ import React from "react"
 
 export default function About(){
     return(
-        <>
+        <React.Fragment>
         <p className="heading">About</p>
         <p className="subsection">I am a frontend developer with a particular interest in making beautiful and artistic websites. I try to keep up with security and best practices, and am always looking for new things to learn</p>
-        </> 
+        </React.Fragment> 
     )
 }
