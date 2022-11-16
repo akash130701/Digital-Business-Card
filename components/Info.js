a68ed794f86c004694ad9bb5adf7c3a4bc6154b2
Akash S <92721700+akash130701@users.noreply.github.com>
2022-11-16 15:24:46 +0530

    Update Info.js
    Fixed a minor bug

--- components/Info.js
+++ components/Info.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Info()
 {
     return(
-        <>
+        <React.Fragment>
         <img className="pfp" src="https://imgs.search.brave.com/lyBIxSFLMx1Mhy2gcMSXuMa0zy6NSBcFtKMQdfO-5Ms/rs:fit:287:300:1/g:ce/aHR0cHM6Ly9ha2Fz/aHNvbWFzdW5kYXJh/bi5maWxlcy53b3Jk/cHJlc3MuY29tLzIw/MjEvMDYvaW1hZ2Ut/NC5wbmc_dz0yODg"/>
         <div className="backdrop">
         </div>
@@ -13,6 +13,6 @@ export default function Info()
         <button type="button" className="btn btn-text"><i className="fa-sharp fa-solid fa-envelope"></i>Email</button>
        
         <button type="button" className="btn-2 btn-text-2"><i className="fa-brands fa-linkedin"></i>LinkedIn</button>
-        </>
+        </React.Fragment>
     )
 }
