76ad4fe3f7ac8c96100804702001abb1c6168698
Akash S <92721700+akash130701@users.noreply.github.com>
2022-11-16 15:25:14 +0530

    Update Interests.js

--- components/Interests.js
+++ components/Interests.js
@@ -1,9 +1,9 @@
 import React from "react"
 export default function Interests() {
     return(
-        <>
+        <React.Fragment>
          <p className="heading2" >Interests</p>
            <p className="subsection2">Table tennis player. Guitarist. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
-        </>
+        </React.Fragment>
     )
 }
